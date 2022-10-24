ee500471a9359fa99c5347f51ea69cd3e1c178a0
Martin Pratoussy <martin.pratoussy@gmail.com>
2022-10-24 11:43:02 +0200

    Added authentication microservice

---
+++ Auth/src/main/java/com/konzik/auth/AuthApplication.java
@@ -0,0 +1,15 @@
+package com.konzik.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
